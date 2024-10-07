b52f9736b0deb76babf6b6a87c3287746c3eab2d
Sean Patton <seanpatton1@outlook.com>
2024-10-07 21:02:27 +0000

    added in about page with images and created menu template page. edits to be done

--- booking/urls.py
+++ booking/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.booking_view, name='booking'),  # Ensure you have a view called 'booking_view'
+    path('', views.booking_view, name='booking'), 
 ]
--- booking/views.py
+++ booking/views.py
@@ -3,13 +3,5 @@ from .models import Booking
 
 # Create your views here.
 
-def booking_list(request):
-    bookings = Booking.objects.all()
-    return render(request, '#', {'bookings': bookings})
-
-def make_booking(request):
-    # Form processing logic here
-    return render(request, '#')
-
 def booking_view(request):
     return render(request, 'booking/booking.html')
